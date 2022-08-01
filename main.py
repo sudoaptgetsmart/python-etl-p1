2994555432ecd51423f0b6cf1b35c392698a38ba
sudoaptgetsmart <96799397+sudoaptgetsmart@users.noreply.github.com>
2022-08-01 16:01:51 -0500

    Fixed trailing semicolon error

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from pgsql import query
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # insert data
-    query(sql.test_insert, ["My Insert!"]);
+    query(sql.test_insert, ["My Insert!"])
 
     # select data
-    results = query(sql.test_select);
+    results = query(sql.test_select)
     print("results: ", results)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
