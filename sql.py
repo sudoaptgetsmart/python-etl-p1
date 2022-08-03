3efd7f739daa1382e43b0d474d7665bdd6704710
sudoaptgetsmart <96799397+sudoaptgetsmart@users.noreply.github.com>
2022-08-03 11:12:39 -0500

    Started create_tables

--- sql.py
+++ sql.py
@@ -1,9 +1,6 @@
-test_select = ('''
-  SELECT *
-  FROM public.test_table;
-''')
-
-test_insert = ('''
-  INSERT INTO public.test_table
-  VALUES(%s);
+create_tables = (''''
+	DROP TABLE IF EXISTS annual_ticket_sales, highest_grossers, popular_creative_types, top_distributors,
+	top_genres, top_grossing_ratings, top_grossing_sources, top_production_methods, wide_releases_count;
+	
+	CREATE TABLE annual_ticket_sales (
 ''')
